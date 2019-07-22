f3a0deea3663292953f8b1e20275573d1629127f
J.U.N <un@JUNurcans-MacBook.local>
2019-07-22 19:01:02 -0400

    show percentage

---
+++ timer.js
@@ -0,0 +1,23 @@
+const waitTime = 3000;
+let currentTime = 0;
+let waitInterval = 10;
+let percent = 0;
+
+function writeWaitingPercentage(percentage) {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`Waiting ... ${percentage}%`);
+}
+const counting = setInterval(function() {
+  currentTime += waitInterval;
+  percent = Math.floor((currentTime / waitTime) * 100);
+  writeWaitingPercentage(percent);
+}, waitInterval);
+
+setTimeout(function() {
+  clearInterval(counting);
+  writeWaitingPercentage(100);
+  process.stdout.write("\n\n\n");
+}, waitTime);
+
+writeWaitingPercentage(percent);
